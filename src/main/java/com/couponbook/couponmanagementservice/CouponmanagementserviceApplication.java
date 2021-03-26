9ee999a500c04f5e7327b90fc7deb0a422c05698
YunchuWang <yunchuwang5@gmail.com>
2021-03-26 00:47:24 -0500

    Project template

---
+++ src/main/java/com/couponbook/couponmanagementservice/CouponmanagementserviceApplication.java
@@ -0,0 +1,13 @@
+package com.couponbook.couponmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponmanagementserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponmanagementserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/couponbook/couponmanagementservice/CouponmanagementserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couponbook.couponmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponmanagementserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
